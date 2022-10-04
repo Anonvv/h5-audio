454da671f3c15ba55dd0c4a99344ac8ce96d70bc
wonderven <wonderven@qq.com>
2022-10-05 01:36:03 +0800

    dev

--- src/components/AudioPlayer.vue
+++ src/components/AudioPlayer.vue
@@ -2,12 +2,10 @@
 	<div class="audio-player">
 		<!-- 音频列表 -->
 		<ul class="audio-list">
-			<li
-				v-for="(item, index) in audioList"
-				:class="index === singeIdx ? 'active' : ''"
-				@dblclick="toggleSing(index)"
-			>
-				{{ item.title }}
+			<li v-for="(item, index) in audioList" :class="index === singeIdx ? 'active' : ''"
+				@dblclick="toggleSing(index)">
+				<span>{{ item.title }}</span>
+				<span v-if="index === singeIdx">playing</span>
 			</li>
 		</ul>
 		<!-- 进度条 -->
@@ -15,7 +13,7 @@
 			<div class="active" :style="{ width: `${playingData.currentTime/playingData.duration*100}%`}"></div>
 		</div>
 		<!-- 播放信息 -->
-		<div class="play-info">
+		<div class="play-panel">
 			<!-- 自定义控件 -->
 			<div class="tools">
 				<span @click="handleCut(0)">&lt;&lt;</span>
@@ -24,9 +22,19 @@
 				</span>
 				<span @click="handleCut(1)">>></span>
 			</div>
-			<!-- 时间 -->
-			<div class="time">
-				{{ secondsToMinites(playingData.currentTime) }}/{{ secondsToMinites(playingData.duration) }}
+			<div class="info">
+				<!-- 时间 -->
+				<span class="time">{{ secondsToMinites(playingData.currentTime) }}/{{
+				secondsToMinites(playingData.duration) }}</span>
+				<!-- 音量 -->
+				<span class="volume">
+					<span>)))</span>
+					<div class="volume-justify">
+						<div class="volume-bar" @mouseup="volumeChange">
+							<div class="active" :style="{height: `${currentVolume/1*100}%`}"></div>
+						</div>
+					</div>
+				</span>
 			</div>
 		</div>
 		<!-- 原生控件，太丑了不需要显示(controls=false)-->
@@ -34,7 +42,7 @@
 	</div>
 </template>
 <script lang="ts" setup>
-import { ref, Ref, reactive, onMounted, computed, watchEffect } from 'vue'
+import { ref, Ref, reactive, onMounted, computed } from 'vue'
 
 const audioList = reactive([ // 音频列表数据
 	{
@@ -55,6 +63,7 @@ const playingData = reactive({ // 当前播放数据
 	url: computed(() => audioList[singeIdx.value].url),
 	title: computed(() => audioList[singeIdx.value].title),
 })
+const currentVolume: Ref<number> = ref(1) // 音量 (0-1)
 
 onMounted(() => {
 	init()
@@ -62,6 +71,7 @@ onMounted(() => {
 
 // 初始化
 const init = () => {
+	currentVolume.value = singeBox.value.volume // 获取默认音量
 	// 播放器事件监听
 	// 当时长有变化时触发，由"NaN"变为实际时长也算
 	singeBox.value.ondurationchange = function () {
@@ -102,7 +112,7 @@ const handlePlay = () => {
  */
 const handleCut = (type: number) => {
 	let index = 0
-	if(type) { // 下一首
+	if (type) { // 下一首
 		if (singeIdx.value < audioList.length - 1) {
 			index = singeIdx.value + 1
 		}
@@ -124,12 +134,19 @@ const toggleSing = (index: number): void => {
 	singeIdx.value = index
 	isPlaying.value = true
 }
-// 监听滚动条的鼠标事件
+// 监听进度条鼠标事件
 const progressChange = (e: any): void => {
 	const percent = e.offsetX / e.target.clientWidth // 计算鼠标位置占总长度的百分比
 	singeBox.value.currentTime = playingData.duration * percent // 按百分比设置已播放时间
-	console.dir(singeBox.value)
 }
+// 监听音量条鼠标事件
+const volumeChange = (e: any): void => {
+	const percent = (e.target.clientHeight - e.offsetY) / e.target.clientHeight // 计算鼠标位置占总高度的百分比
+	// 按百分比设置音量
+	currentVolume.value = 1 * percent
+	singeBox.value.volume = currentVolume.value
+}
+
 /**
  * 格式化秒数
  * @param totalSeconds 秒
@@ -144,28 +161,33 @@ const secondsToMinites = (totalSeconds: number = 0): string => {
 </script>
 <style lang="sass" scoped>
 .audio-player 
-	padding: 2rem
+	padding: 6rem
 	color: #fff
-	background-color: rgba(black, .4)
 	.audio-list
 		list-style: none
 		li
+			display: flex
+			justify-content: space-between
 			line-height: 4.2rem
+			padding: 0 1rem
 			cursor: pointer
 			user-select: none
+			&:hover
+				background-color: rgba(black, .2)
 			&.active
 				color: #2bf
+				background-color: rgba(black, .2)
 	.progress
 		height: 2px
 		margin-top: 2rem
-		background-color: #898989
+		background-color: #ddd
 		position: relative
 		cursor: pointer
 		.active
 			position: absolute
 			height: 100%
 			background-color: #2bf
-	.play-info
+	.play-panel
 		padding: 1rem 0
 		display: flex
 		justify-content: space-between
@@ -176,4 +198,43 @@ const secondsToMinites = (totalSeconds: number = 0): string => {
 				cursor: pointer
 				&:hover
 					color: #2bf
+		.info
+			font-size: 1.4rem
+			position: relative
+			.time
+				padding-right: 4rem
+			.volume
+				cursor: pointer
+				position: absolute
+				bottom: 0
+				right: 1rem
+				&:hover
+					.volume-justify
+						height: 100px
+						padding: 1rem
+				.volume-justify
+					display: flex
+					justify-content: center
+					position: absolute
+					right: 0rem
+					bottom: 2rem
+					width: 30px
+					height: 0
+					padding: 0
+					border-radius: 3px
+					background-color: rgba(black, .2)
+					.volume-bar
+						width: 4px
+						height: 100%
+						background-color: #ddd
+						border-radius: 3px
+						position: relative
+						overflow: hidden
+						.active
+							position: absolute
+							bottom: 0
+							width: 100%
+							background-color: #2bf
 </style>
+
+
